3cff9e68aba39ea5d2709c92281945cabe1d1fbe
Suzy Soares Campos <suzyscampos@hotmail.com>
2024-09-06 16:15:54 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+function pesquisar() {
+    let section = document.getElementById("resultados-pesquisa");
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+    if (!campoPesquisa) {
+        section.innerHTML = "<p>Nada foi encontrado</p>";
+        return;
+    }
+
+    let resultados = "";
+
+    for (let dado of dados) {
+        if (dado.titulo.includes(campoPesquisa) || dado.descricao.includes(campoPesquisa) || dado.tags.includes(campoPesquisa)) {
+            resultados += `
+                <div class="item-resultado">
+                    <h2>
+                        <a href="#" target="_blank">${dado.titulo}</a>
+                    </h2>
+                    <p class="descricao-meta">${dado.descricao}</p>
+                    <a href=${dado.link} target="_blank">Mais informações</a>
+                </div>
+            `;
+        }
+    }
+
+    if (!resultados) {
+        resultados = "<p>Nada foi encontrado</p>"; 
+    }
+
+    section.innerHTML = resultados;
+}
